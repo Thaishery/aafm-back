3484e067192e617e61b35fe62a721af8fee4232b
Thaishery <gdeb@gdeb.fr>
2024-03-04 01:23:19 +0100

    fix JWT config

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -2,5 +2,6 @@
 cd /var/www/symfony_docker/aafm-back/
 # chmod +x ./bin/console
 # Run migrations
+php bin/console lexik:jwt:generate-keypair --skip-if-exists
 make tests
 symfony console doctrine:migrations:migrate --no-interaction
