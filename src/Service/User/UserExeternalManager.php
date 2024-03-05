cca9760737b3adbcb66e30f7a344803dad35eeb8
Thaishery <gdeb@gdeb.fr>
2024-03-05 17:32:35 +0100

    some cleaning

--- src/Service/User/UserExeternalManager.php
+++ src/Service/User/UserExeternalManager.php
@@ -9,22 +9,6 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 
 class UserExternalManager{
-      // {
-    //   "iss": "https://accounts.google.com",
-    //   "azp": "436223154606-lq7257b6v49dvkjnvir7pirrm4k9lmjb.apps.googleusercontent.com",
-    //   "aud": "436223154606-lq7257b6v49dvkjnvir7pirrm4k9lmjb.apps.googleusercontent.com",
-    //   "sub": "102303421889567174010",
-    //   "email": "dykers@gmail.com",
-    //   "email_verified": true,
-    //   "at_hash": "Mx-cuPH_O9s2HeE-qpowqA",
-    //   "name": "Guillaume a",
-    //   "picture": "https://lh3.googleusercontent.com/a/ACg8ocJQkYCFj8jbK7CDEJj3iF0fVhnyxYHSt0WLGU4Hh1GSLRU=s96-c",
-    //   "given_name": "Guillaume",
-    //   "family_name": "a",
-    //   "locale": "fr",
-    //   "iat": 1709632669,
-    //   "exp": 1709636269
-    // }
 
   public function createOrPrepareExternalUser($userInfos,UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $manager,JWTTokenManagerInterface $jwtManager){
     $existUser = $manager->getRepository(User::class)->findOneBy(['email'=>$userInfos->email]);
@@ -69,31 +53,6 @@ class UserExternalManager{
     //return result : 
     return $response;
   }
-  // public function createOrLogExternalUser($userInfos,UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $manager,JWTTokenManagerInterface $jwtManager){
-  //   $existUser = $manager->getRepository(User::class)->findOneBy(['email'=>$userInfos->email]);
-  //   if($existUser) return [
-  //     'user'=>$existUser->getUserIdentifier(),
-  //     'token'=>$this->getToken($existUser,$jwtManager)
-  //   ];
-  //   $user = new User(); 
-  //   if(empty($userInfos->email)) return false;
-  //   $user->setIsInternal(false);
-  //   $user->setRoles(['ROLE_USER']);
-  //   $user->setEmail($userInfos->email);
-  //   $generatedPassword = $this->generateRandomPassword();
-  //   $user->setPassword($passwordHasher->hashPassword($user, $generatedPassword));
-  //   unset($generatedPassword);
-  //   if(!empty($userInfos->family_name)) $user->setFirstname($userInfos->family_name);
-  //   if(!empty($userInfos->given_name)) $user->setLastname($userInfos->given_name);
-  //   $manager->persist($user);
-  //   $manager->flush();
-  //   $user = $manager->getRepository(User::class)->findOneBy(['email'=>$userInfos->email]);
-  //   if($existUser) return [
-  //     'user'=>$existUser->getUserIdentifier(),
-  //     'token'=>$this->getToken($existUser,$jwtManager)
-  //   ];
-  //   return false;
-  // }
 
   public function getToken($user, JWTTokenManagerInterface $jwtManager){
     // $jwtManager = new JWTTokenManagerInterface();
